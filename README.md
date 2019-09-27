a8e4ad4d0b791c213591bb99ce166c1b04663cf3
fsaint <fsaint@gmail.com>
2019-09-27 13:11:35 -0400

    Added Inkhat

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # piscripts
+
 Some Scripts for the Raspberry PI
+
+# Convert images for InkHat
+
+convert keystrokes.png -define png:bit-depth=8 -define png:color-type=3 keystrokes_bw.png
---
+++ render_keys.py
@@ -0,0 +1,15 @@
+from inky import InkyWHAT
+from PIL import Image
+
+inkywhat = InkyWHAT('red')
+
+
+#img = Image.open("InkywHAT-400x300.png")
+img = Image.open("keystrokes_bw.png")
+
+inkywhat.set_image(img)
+
+inkywhat.show()
+
+
+
