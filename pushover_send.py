bb5d53b8d9213845a01172f4830db85665c95252
Felipe Saint-jean <fsaint@office.local>
2019-10-04 11:43:45 -0400

    Added push semd

---
+++ pushover_send.py
@@ -0,0 +1,33 @@
+from pushover import Client
+from weasyprint import HTML, CSS
+import sys
+user  = 'u88o975vp68jt3gaoyhfny9uq4cp4c'
+token = 'ar3tggesdyxaaz7beq1wxwftscshsk'
+
+from weasyprint.fonts import FontConfiguration
+
+
+def genimage():
+    #font_config = FontConfiguration()
+    html = HTML(string='<h1>The title</h1>')
+    #css = CSS(string="""@font-face {font-family: Gentium;src: url(http://example.com/fonts/Gentium.otf);}h1 { font-family: Gentium }""",font_config=font_config)
+    return html.write_png()
+
+
+
+
+def alert(text, **kwargs):
+    """
+    >>> alert("Doctest",title="Title", sound="magic",url="https://school4one.com/",attachment=genimage(),url_title="School 4 One Link")
+    'OK'
+    """
+    client = Client(user, api_token=token)
+    client.send_message(text, html=1 , **kwargs)
+    return 'OK'
+
+if __name__ == "__main__":
+    #import doctest
+    #doctest.testmod()
+    lst = sys.argv[2:]
+    args = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)} 
+    alert(sys.argv[1], **args)
