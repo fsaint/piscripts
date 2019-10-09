2ce250a1d99f8fc7c4e1b4486c9d58352c8adecd
fsaint <fsaint@gmail.com>
2019-10-09 17:58:04 -0400

    Added update

--- camera.py
+++ camera.py
@@ -3,6 +3,13 @@ from picamera import PiCamera
 
 from send import alert 
 
+import socket
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+
 camera = PiCamera()
 camera.resolution = (1024, 768)
 #camera.start_preview()
@@ -11,4 +18,4 @@ sleep(2)
 camera.capture('foo.jpg')
 
 
-alert("Doctest",title="Title", sound="magic",attachment=open("foo.jpg","rb"),url_title="School 4 One Link")
+alert("Doctest",title=f"IP: {get_ip_address()}", sound="magic",attachment=open("foo.jpg","rb"),url_title="School 4 One Link")
