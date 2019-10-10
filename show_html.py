64cf63e62cf51686e907a9070281b4e7d5afb878
fsaint <fsaint@gmail.com>
2019-10-10 18:04:55 -0400

    Added my show html

--- show_html.py
+++ show_html.py
@@ -2,32 +2,29 @@ from weasyprint import HTML, CSS
 from PIL import Image
 import io
 from inky import InkyWHAT
+import markdown2
+md = """
+#Felipe ... this is a test 
 
-html = """
-<h1>   Felipe ... this is a test </h1>
-<table>
-<tr>
-   <td>
-       A1 ⌘ ⌥ ⌫ ⇧
-   </td>
-   <td>
-       A2
-   </td>
-</tr>
-</table>
+|Action  |Key  |
+|:------:|:-----:|
+|▛      | ^⌥K |
+|▜      | ^⌥J |
+
+
 """
-
+html = markdown2.markdown(md, extras = ['tables'])
 css = """
 @page {size: 400px 300px; margin:0;}
 table *
 {
    font-size: 1em !important;
    color: #000 !important;
-   font-family: Arial !important;
+   font-family: "Arial Black", Gadget, sans-serif !important;
    font-weight: 900;
 }
 table {
-    width: 100%;
+    width: 50%;
     height: 100%;
     border: 1px solid black;
 }
