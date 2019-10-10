83e92b1b0d09dc0ed153758ee2ec6509f747c0e8
fsaint <fsaint@gmail.com>
2019-10-10 10:11:42 -0400

    Some updates

--- camera.py
+++ camera.py
@@ -1,21 +1,33 @@
 from time import sleep
 from picamera import PiCamera
-
+from PIL import Image
 from send import alert 
-
+import io
 import socket
+import time
+from inky import InkyWHAT
+
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     return s.getsockname()[0]
 
+stream = io.BytesIO()
 
-camera = PiCamera()
-camera.resolution = (1024, 768)
+with PiCamera() as camera:
+    camera.resolution = (1024, 768)
+    #camera.start_preview()
+    time.sleep(2)
+    camera.capture(stream, format='jpeg')
 #camera.start_preview()
 # Camera warm-up time
-sleep(2)
-camera.capture('foo.jpg')
+stream.seek(0)
+image = Image.open(stream)
 
+inkywhat = InkyWHAT('red')
 
-alert("Doctest",title=f"IP: {get_ip_address()}", sound="magic",attachment=open("foo.jpg","rb"),url_title="School 4 One Link")
+inkywhat.set_image(image.resize([400,300]).convert('P'))
+inkywhat.show()
+imgByteArr = io.BytesIO()
+image.resize([400,300]).convert('P').save(imgByteArr, format='PNG')
+alert("Doctest",title=f"IP: {get_ip_address()}", sound="magic",attachment=imgByteArr.getvalue(),url_title="School 4 One Link")
--- show_html.py
+++ show_html.py
@@ -3,11 +3,38 @@ from PIL import Image
 import io
 from inky import InkyWHAT
 
+html = """
+<h1>   Felipe ... this is a test </h1>
+<table>
+<tr>
+   <td>
+       A1 ⌘ ⌥ ⌫ ⇧
+   </td>
+   <td>
+       A2
+   </td>
+</tr>
+</table>
+"""
+
+css = """
+@page {size: 400px 300px; margin:0;}
+table *
+{
+   font-size: 1em !important;
+   color: #000 !important;
+   font-family: Arial !important;
+   font-weight: 900;
+}
+table {
+    width: 100%;
+    height: 100%;
+    border: 1px solid black;
+}
+"""
 inkywhat = InkyWHAT('red')
-
-html = HTML(string='<h1>The title</h1>')
-png = html.write_png()
-image = Image.open(io.BytesIO(png)).resize([400,300]).convert('1')
-
+html = HTML(string=html)
+png = html.write_png(stylesheets = [CSS(string=css)])
+image = Image.open(io.BytesIO(png)).resize([400,300]).convert('P')
 inkywhat.set_image(image)
 inkywhat.show()
