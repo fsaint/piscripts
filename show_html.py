7e6172d394fd846f375bacb3b17f08687f3807f8
Felipe Saint-jean <fsaint@osxcache.pvt.lan>
2019-10-10 22:13:06 -0400

    Finalized codes

--- show_html.py
+++ show_html.py
@@ -1,21 +1,34 @@
 from weasyprint import HTML, CSS
 from PIL import Image
 import io
-from inky import InkyWHAT
 import markdown2
-md = """
-#Felipe ... this is a test 
 
-|Action  |Key  |
+def sq(top = 0, left = 0, width = 6, height = 5 ):
+     return f"""
+<div style="width: 12px; height: 12px; border: 1px solid black;">
+<div style="position: relative; top: {top}px; left: {left}px;width: {width}px; height: {height}px; background-color: black;"></div>
+</div>
+""".replace("\n","")
+     
+md = f"""
+|Act  |Key  |
 |:------:|:-----:|
-|▛      | ^⌥K |
-|▜      | ^⌥J |
-
+|{sq(top = 0, width = 6, height = 6, left = 0)}      | ^⌥U |
+|{sq(top = 0, width = 6, height = 6, left = 6)}      | ^⌥I |
+|{sq(top = 6, width = 6, height = 6, left = 6)}      | ^⌥K |
+|{sq(top = 6, width = 6, height = 6, left = 0)}      | ^⌥J |
+|{sq(top = 0, width = 4, height = 12, left = 0)}      | ^⌥D |
+|{sq(top = 0, width = 8, height = 12, left = 4)}      | ^⌥T |
+|Clip | ⇧⌃⌘ 4 |
 
 """
+print(md)
 html = markdown2.markdown(md, extras = ['tables'])
 css = """
 @page {size: 400px 300px; margin:0;}
+body {
+   background-color: white;
+}
 table *
 {
    font-size: 1em !important;
@@ -24,14 +37,19 @@ table *
    font-weight: 900;
 }
 table {
-    width: 50%;
+    width: 33%;
     height: 100%;
     border: 1px solid black;
 }
 """
-inkywhat = InkyWHAT('red')
 html = HTML(string=html)
 png = html.write_png(stylesheets = [CSS(string=css)])
-image = Image.open(io.BytesIO(png)).resize([400,300]).convert('P')
-inkywhat.set_image(image)
-inkywhat.show()
+image = Image.open(io.BytesIO(png)).convert('P')
+#image = Image.open(io.BytesIO(png)).resize([400,300]).convert('P')
+try:
+   from inky import InkyWHAT
+   inkywhat = InkyWHAT('red')
+   inkywhat.set_image(image)
+   inkywhat.show()
+except:
+   image.save("result.png")
